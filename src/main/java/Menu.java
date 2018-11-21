30a0cd8c8c04db45bfa94acb9b383d4712b549e4
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-21 08:44:56 +0100

    Adding Menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Menu {
+
+    public String showMenu(){
+        String menuString;
+
+
+        menuString =""
+                        + "============================================================\n"
+                        + "||     Welcome to your personal financial analyzer        ||          {\n"
+                        + "============================================================       {   }\n"
+                        + "||   Choose an operation:                                 ||        }_ { __{\n"
+                        + "||        1. Add an Expense                               ||     .'{    }   }'-.\n"
+                        + "||        2. Modify Your Expense                          ||    (   }       {   )\n"
+                        + "||        3. Delete Your Expense                          ||    |`-.._______..-'|\n"
+                        + "||        4. Add a Category for Your Expenses             ||    |               ;--.\n"
+                        + "||        5. Delete a Category                            ||    |              (__  \\\n"
+                        + "||        6. Display Expenses per Category                ||    |               | )  )\n"
+                        + "||        7. Display all Expenses in a given period       ||    |               |/  /\n"
+                        + "||        8. Define Budget                                ||    |               /  /\n"
+                        + "||        9. Set up upper limit restriction               ||    |              (  /\n"
+                        + "============================================================    \\               y'\n"
+                        + "                                                                 `-.._______..-'\n";
+        return menuString;
+    }
+
+
+
+
+
 }
