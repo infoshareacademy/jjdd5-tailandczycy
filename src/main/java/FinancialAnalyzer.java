c9b3dc36a3ff66e7f9ed9100ac5280556b3b7ecd
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-21 15:05:25 +0100

    updating menu

---
+++ src/main/java/FinancialAnalyzer.java
@@ -0,0 +1,8 @@
+public class FinancialAnalyzer {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        System.out.println(menu.showMenu());
+    }
+
+}
---
+++ src/main/java/Menu.java
@@ -0,0 +1,72 @@
+public class Menu {
+
+    private String menuString;
+
+
+
+    public String showMenu(){
+
+
+        menuString =
+                        "\n  MMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEENNNNNNNN        NNNNNNNNUUUUUUUU     UUUUUUUU\n"
+                        + "  M:::::::M             M:::::::ME::::::::::::::::::::EN:::::::N       N::::::NU::::::U     U::::::U\n"
+                        + "  M::::::::M           M::::::::ME::::::::::::::::::::EN::::::::N      N::::::NU::::::U     U::::::U\n"
+                        + "  M:::::::::M         M:::::::::MEE::::::EEEEEEEEE::::EN:::::::::N     N::::::NUU:::::U     U:::::UU\n"
+                        + "  M::::::::::M       M::::::::::M  E:::::E       EEEEEEN::::::::::N    N::::::N U:::::U     U:::::U\n"
+                        + "  M:::::::::::M     M:::::::::::M  E:::::E             N:::::::::::N   N::::::N U:::::D     D:::::U\n"
+                        + "  M:::::::M::::M   M::::M:::::::M  E::::::EEEEEEEEEE   N:::::::N::::N  N::::::N U:::::D     D:::::U\n"
+                        + "  M::::::M M::::M M::::M M::::::M  E:::::::::::::::E   N::::::N N::::N N::::::N U:::::D     D:::::U\n"
+                        + "  M::::::M  M::::M::::M  M::::::M  E:::::::::::::::E   N::::::N  N::::N:::::::N U:::::D     D:::::U\n"
+                        + "  M::::::M   M:::::::M   M::::::M  E::::::EEEEEEEEEE   N::::::N   N:::::::::::N U:::::D     D:::::U\n"
+                        + "  M::::::M    M:::::M    M::::::M  E:::::E             N::::::N    N::::::::::N U:::::D     D:::::U\n"
+                        + "  M::::::M     MMMMM     M::::::M  E:::::E       EEEEEEN::::::N     N:::::::::N U::::::U   U::::::U\n"
+                        + "  M::::::M               M::::::MEE::::::EEEEEEEE:::::EN::::::N      N::::::::N U:::::::UUU:::::::U\n"
+                        + "  M::::::M               M::::::ME::::::::::::::::::::EN::::::N       N:::::::N  UU:::::::::::::UU\n"
+                        + "  M::::::M               M::::::ME::::::::::::::::::::EN::::::N        N::::::N    UU:::::::::UU\n"
+                        + "  MMMMMMMM               MMMMMMMMEEEEEEEEEEEEEEEEEEEEEENNNNNNNN         NNNNNNN      UUUUUUUUU\n"
+                        + "==============================================================================================\n"
+                        + "||     Welcome to your personal financial analyzer        ||          {\n"
+                        + "============================================================       {   }\n"
+                        + "||   Choose an operation:                                 ||        }_ { __{\n"
+                        + "||        1. Add an Expense                               ||     .'{    }   }'-.\n"
+                        + "||        2. Modify Your Expense                          ||    (   }       {   )\n"
+                        + "||        3. Delete Your Expense                          ||    |`-.._______..-'|\n"
+                        + "||        4. Add a Category for Your Expenses             ||    |               ;--.\n"
+                        + "||        5. Delete a Category                            ||    |              (__  \\\n"
+                        + "||        6. Display Expenses per Category                ||    |               | )  )\n"
+                        + "||        7. Display all Expenses in a given period       ||    |               |/  /\n"
+                        + "||        8. Define Budget                                ||    |               /  /\n"
+                        + "||        9. Set up upper limit restriction               ||    |              (  /\n"
+                        + "============================================================    \\               y'\n"
+                        + "                                                                 `-.._______..-'\n";
+        return menuString;
+    }
+
+//    public void chooseMenu(int option){
+//
+//        switch(option){
+//            case 1:
+//                addExpense();
+//                break;
+//            case 2:
+//                modifyExpense();
+//                break;
+//            case 3:
+//                deleteExpense();
+//                break;
+//            case 4:
+//                addCategory();
+//                break;
+//            case 5:
+//                displayExpensePerCategory();
+//            case 6:
+//                displayAllExpenses();
+//                break;
+//            case 7:
+//                defineBudget();
+//            case 8:
+//                setUpLimit();
+//        }
+//    }
+
+}
