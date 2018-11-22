cec83eed26e4b13505fae7a07fcf63916fc5f33f
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-22 08:51:43 +0100

    Adding Main

---
+++ src/main/java/com/infoshareacademy/tailandczycy/console/Main.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy.tailandczycy.console;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StartApp start = new StartApp();
+        start.runApp();
+    }
+
+}
