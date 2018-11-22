fbd357480a57dba97e8aa6c7759aeccceff134ae
mikolajCygert <miki@comsite.com.pl>
2018-11-22 15:24:10 +0100

    update

--- src/main/java/com/infoshareacademy/tailandczycy/console/Main.java
+++ src/main/java/com/infoshareacademy/tailandczycy/console/Main.java
@@ -1,12 +1,9 @@
 package com.infoshareacademy.tailandczycy.console;
 
-import com.infoshareacademy.tailandczycy.data.Parser;
-
-import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
         Menu menu = new Menu();
         System.out.println(menu.showMenu());
     }
