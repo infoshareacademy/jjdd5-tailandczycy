96d6a578fc0363a62603fd57044136512a5f2cda
Mikołaj Cygert <32238756+MCygert@users.noreply.github.com>
2018-11-22 15:52:12 +0100

    Jd5ta-2 (#2)
    * Added gitignore file
    
    * Added maven and gson Dependecies
    
    * initialization and reading from a JSON file
    
    * changing module name
    
    * Adding Budget class
    
    * Adding Menu
    
    * Adding imports
    
    * updating menu
    
    * menu merge conflicts
    
    * improving structure
    
    * adding console package
    
    * refactoring
    
    * Refactorized code
    
    * JD5TA-2 added reading from file and changed date format to yyyy-MM-dd
    
    * JD5TA-2 Changed Structure of Parser class
    
    * Reading from file
    
    * Added reading from file
    
    * Deleted .idea
    
    * update pom.xml
    
    * update menu
    
    * Update
    
    * Update
    
    * Test
    
    * update
    
    * update
    
    * removed conflicts
    
    * Deleted blank lines
    
    * deleted blank lines
    
    * deleted blank lines
    
    * Deleted not important class

--- src/main/java/com/infoshareacademy/tailandczycy/service/Budget.java
+++
@@ -1,8 +0,0 @@
-package com.infoshareacademy.tailandczycy.service;
-
-import java.math.BigDecimal;
-
-public class Budget {
-
-    private BigDecimal totalBudget;
-}
---
+++ src/main/java/com/infoshareacademy/tailandczycy/service/Expense.java
@@ -0,0 +1,75 @@
+package com.infoshareacademy.tailandczycy.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Expense {
+    private String category;
+    private String comment;
+    private BigDecimal amount;
+    private LocalDate date;
+    public Expense(String category, String comment, BigDecimal amount, String date) {
+        this.category = category;
+        this.comment = comment;
+        this.amount = amount;
+        this.date = LocalDate.parse(date);
+    }
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = LocalDate.parse(date);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Expense expense = (Expense) o;
+        return Objects.equals(category, expense.category) &&
+                Objects.equals(comment, expense.comment) &&
+                Objects.equals(amount, expense.amount) &&
+                Objects.equals(date, expense.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(category, comment, amount, date);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "category='" + category + '\'' +
+                ", comment='" + comment + '\'' +
+                ", amount=" + amount +
+                ", date=" + date +
+                '}';
+    }
+}
