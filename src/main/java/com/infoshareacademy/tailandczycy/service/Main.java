4c7a18848fe162eef3705759729eeaa92d30ba93
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-24 14:29:47 +0100

    refactor

--- src/main/java/com/infoshareacademy/tailandczycy/service/Main.java
+++ src/main/java/com/infoshareacademy/tailandczycy/service/Main.java
@@ -1,7 +1,5 @@
 package com.infoshareacademy.tailandczycy.service;
 
-import com.infoshareacademy.tailandczycy.console.StartApp;
-
 public class Main {
 
     public static void main(String[] args) {
