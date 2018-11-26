4a9fcdb6bad945318531f8c0c0753b5249494ad8
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-26 16:53:11 +0100

    adding Category

--- src/main/java/com/infoshareacademy/tailandczycy/service/Category.java
+++ src/main/java/com/infoshareacademy/tailandczycy/service/Category.java
@@ -1,4 +1,25 @@
 package com.infoshareacademy.tailandczycy.service;
 
+import java.math.BigDecimal;
+
 public class Category {
+
+    private final String name;
+    private BigDecimal limit;
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
 }
