2de0ba65b6ec380d5601036a4a895b50a2b20ba1
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-29 16:20:26 +0100

    upgrading FileOperations class

--- src/main/java/com/infoshareacademy/tailandczycy/data/FileOperations.java
+++ src/main/java/com/infoshareacademy/tailandczycy/data/FileOperations.java
@@ -2,43 +2,50 @@ package com.infoshareacademy.tailandczycy.data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import com.infoshareacademy.tailandczycy.service.Category;
 import com.infoshareacademy.tailandczycy.service.Expense;
-
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class FileOperations {
 
-    public static void main(String[] args) throws IOException{
-        final Path originalFile=Paths.get("data","test.json");
+    private final Path expenseFile=Paths.get("data","expenses.json");
+    private final Path categoryFile=Paths.get("data", "categories.json");
 
+    public List<Category> deserializeListOfCategories() throws IOException{
 
-        List<String>lines=Files.readAllLines(originalFile);
-        String json = lines.stream()
-                .collect(Collectors.joining());
+        String json = getStringFromFile(categoryFile);
+        Gson gson = new Gson();
+        Category[] array = gson.fromJson(json, Category[].class);
+        return Arrays.asList(array);
+    }
 
+    public List<Expense> deserializeListOfExpense() throws IOException {
 
+        String json = getStringFromFile(expenseFile);
         Gson gson = new GsonBuilder()
                 .registerTypeHierarchyAdapter(LocalDate.class, new GsonLocalDateTypeAdapter())
                 .create();
         Expense[] array = gson.fromJson(json, Expense[].class);
-        List<Expense>expenses = Arrays.asList(array);
-        System.out.println(expenses.get(0).getComment());
+        return Arrays.asList(array);
     }
 
-//    private final Path originalFile=Paths.get("data","test.json");
-//
-//    public void foo() throws IOException {
-//            List<String>lines=Files.readAllLines(originalFile);
-//            String json = lines.stream()
-//                    .collect(Collectors.joining());
-//        }
+    private String getStringFromFile(Path file) throws IOException {
+        List<String> lines = Files.readAllLines(file);
+        return String.join("", lines);
+    }
+
+    public List<Expense> getListOfExpenses() throws IOException{
+        return deserializeListOfExpense();
+    }
+
+    public List<Category> getListOfCategories() throws IOException{
+        return deserializeListOfCategories();
+    }
 }
