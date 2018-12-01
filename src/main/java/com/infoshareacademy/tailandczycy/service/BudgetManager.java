5e3d9a9d46f03b33ed47df3f7993c8e848e819b2
Mikołaj Cygert <miki@comsite.com.pl>
2018-12-01 19:08:32 +0100

    JD5TA-13 Almost done with AddingExpense just creating and saving object of expense

--- src/main/java/com/infoshareacademy/tailandczycy/service/BudgetManager.java
+++ src/main/java/com/infoshareacademy/tailandczycy/service/BudgetManager.java
@@ -1,16 +1,18 @@
 package com.infoshareacademy.tailandczycy.service;
 
+import com.infoshareacademy.tailandczycy.console.ConsoleReader;
 import com.infoshareacademy.tailandczycy.data.dao.CategoryDao;
 import com.infoshareacademy.tailandczycy.data.dao.ExpenseDao;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class BudgetManager {
-
+    private ConsoleReader consoleReader = new ConsoleReader();
     private ExpenseDao expenseDao = new ExpenseDao();
     private CategoryDao categoryDao = new CategoryDao();
     private Budget budgetDao = new Budget();
@@ -55,9 +57,26 @@ public class BudgetManager {
         return category;
     }
 
-    public BigDecimal getActualBudget(){
+    public BigDecimal getActualBudget() {
         return budgetDao.getBudget().subtract(expenseDao.getAll().stream()
                 .map(Expense::getAmount)
-                .reduce(new BigDecimal(0) , BigDecimal::add));
+                .reduce(new BigDecimal(0), BigDecimal::add));
+    }
+
+    public List<String> gettingCategoriesFromUser() {
+        boolean allCategories = true;
+        List<String> categories = new ArrayList<>();
+        while (allCategories) {
+            System.out.println("Define category: ");
+            categories.add(consoleReader.readString());
+            System.out.println("Are those all categories?");
+            System.out.println("If not click n otherwise y");
+            String flag = consoleReader.readString();
+            if (flag.equalsIgnoreCase("y")) {
+                allCategories = false;
+            }
+
+        }
+        return categories;
     }
 }
