89291594ab58ea06480109477fd90ea2ddd85067
Mateusz Odroniec <mateusz.odr@gmail.com>
2018-12-12 16:58:01 +0100

    Add template

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/TestServlet.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/TestServlet.java
@@ -26,6 +26,8 @@ public class TestServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+
         Map<String, Object> model = new HashMap<>();
         model.put("date", LocalDateTime.now());
 
