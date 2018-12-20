9638efced5cda584e91da0b774f6247d15318e81
mikolajCygert <miki@comsite.com.pl>
2018-12-20 16:48:39 +0100

    Feature/JD5TA-18 Delete Expense

--- finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/DeleteExpenseServlet.java
+++ finance-manager-web/src/main/java/com/infoshareacademy/tailandczycy/web/DeleteExpenseServlet.java
@@ -1,13 +1,7 @@
 package com.infoshareacademy.tailandczycy.web;
 
-
-import com.infoshareacademy.tailandczycy.dao.ExpenseDao;
 import com.infoshareacademy.tailandczycy.dto.ExpenseRequestViewDto;
-import com.infoshareacademy.tailandczycy.freemarker.TemplateProvider;
-import com.infoshareacademy.tailandczycy.model.Expense;
 import com.infoshareacademy.tailandczycy.views.ExpenseRequestView;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -16,9 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @WebServlet("/delete-expense")
@@ -26,20 +17,22 @@ public class DeleteExpenseServlet extends HttpServlet {
     private static final String TEMPLATE_NAME = "delete-expense";
     private final Logger logger = Logger.getLogger(getClass().getName());
     private static final String TEMPLATE_EXPENSE_LIST = "expense-list";
+    @Inject
+    ExpenseRequestViewDto expenseRequestViewDto;
 
     @Inject
-    private ExpenseDao expenseDao;
+    ExpenseRequestView expenseRequestView;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.addHeader("Content-Type", "text/html; charset=utf-8");
         Long id = Long.parseLong(req.getParameter("id"));
-        Expense expense = expenseDao.findById(id);
-        handleResponse(resp, expense.getId());
+        expenseRequestView = expenseRequestViewDto.getExpenseById(id);
+        handleResponse(resp, expenseRequestView.getId());
     }
 
     private void handleResponse(HttpServletResponse resp, Long id) throws IOException {
-        expenseDao.delete(id);
+        expenseRequestViewDto.deleteExpense(id);
         resp.sendRedirect(TEMPLATE_EXPENSE_LIST);
     }
 }
